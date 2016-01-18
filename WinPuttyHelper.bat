dd537d8909adbb076ce3e3d89e2e61160cb792cc
Nick Farrow <farrow.nick@gmail.com>
2016-01-18 01:39:55 -0600

    adding commits
    adding commits

--- WinPuttyHelper.bat
+++ WinPuttyHelper.bat
@@ -1,8 +1,16 @@
 @echo off
+
+REM set Putty local path
 set PPATH="C:\YOUR_PUTTY_PATH"
+REM Set the server name or IP address you wanting to connect
 set SEV=SERVER_NAME
+REM Set remote servers ser name that will be used to connect
 set USR=USER_NAME
+REM Set key authentication file to login with. THIS IS NOT A PASSWORD FILE!!!!!
 set LKEY=private.ppk
+REM Set full path and filename for commands you want to run on the remote server.
 set PCMDS=post_login_cmds.txt
+
+REM start script
 cd %PPATH%
 putty.exe -ssh %USR%@%SEV% -i %LKEY% -m %PCMDS%
